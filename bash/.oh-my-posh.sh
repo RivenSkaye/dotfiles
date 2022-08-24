da53d2bc70d2060394f3a2edd6addb185504135e
Riven Skaye <riven@tae.moe>
2022-08-24 10:38:07 +0200

    generated from WSL

--- bash/.oh-my-posh.sh
+++ bash/.oh-my-posh.sh
@@ -1,41 +1,30 @@
-export POSH_THEME='/home/Riven/.iterm2.omp.json'
+export POSH_THEME=/home/riven/.iterm2.omp.json
 export POWERLINE_COMMAND="oh-my-posh"
 export CONDA_PROMPT_MODIFIER=false
-
-TIMER_START="/tmp/${USER}.start.$$"
-
-# some environments don't have the filesystem we'd expect
-if [[ ! -d "/tmp" ]]; then
-  TIMER_START="${HOME}/.${USER}.start.$$"
-fi
+omp_start_time=""
 
 # start timer on command start
-PS0='$(C:/Users/Riven/AppData/Local/Programs/oh-my-posh/bin/oh-my-posh.exe get millis > "$TIMER_START")'
+PS0='${omp_start_time:0:$((omp_start_time="$(_omp_start_timer)",0))}'
 # set secondary prompt
-PS2="$(C:/Users/Riven/AppData/Local/Programs/oh-my-posh/bin/oh-my-posh.exe print secondary --config="$POSH_THEME" --shell=bash --shell-version="$BASH_VERSION")"
+PS2="$(/usr/bin/oh-my-posh print secondary --config="$POSH_THEME" --shell=bash --shell-version="$BASH_VERSION")"
+
+function _omp_start_timer() {
+    /usr/bin/oh-my-posh get millis
+}
 
 function _omp_hook() {
     local ret=$?
-
-    omp_stack_count=$((${#DIRSTACK[@]} - 1))
-    omp_elapsed=-1
-    if [[ -f "$TIMER_START" ]]; then
-        omp_now=$(C:/Users/Riven/AppData/Local/Programs/oh-my-posh/bin/oh-my-posh.exe get millis)
-        omp_start_time=$(cat "$TIMER_START")
+    local omp_stack_count=$((${#DIRSTACK[@]} - 1))
+    local omp_elapsed=-1
+    if [[ -n "$omp_start_time" ]]; then
+        local omp_now=$(/usr/bin/oh-my-posh get millis)
         omp_elapsed=$((omp_now-omp_start_time))
-        rm -f "$TIMER_START"
+        omp_start_time=""
     fi
-    PS1="$(C:/Users/Riven/AppData/Local/Programs/oh-my-posh/bin/oh-my-posh.exe print primary --config="$POSH_THEME" --shell=bash --shell-version="$BASH_VERSION" --error="$ret" --execution-time="$omp_elapsed" --stack-count="$omp_stack_count" | tr -d '\0')"
-
+    PS1="$(/usr/bin/oh-my-posh print primary --config="$POSH_THEME" --shell=bash --shell-version="$BASH_VERSION" --error="$ret" --execution-time="$omp_elapsed" --stack-count="$omp_stack_count" | tr -d '\0')"
     return $ret
 }
 
-if [ "$TERM" != "linux" ] && [ -x "$(command -v C:/Users/Riven/AppData/Local/Programs/oh-my-posh/bin/oh-my-posh.exe)" ] && ! [[ "$PROMPT_COMMAND" =~ "_omp_hook" ]]; then
+if [ "$TERM" != "linux" ] && [ -x "$(command -v /usr/bin/oh-my-posh)" ] && ! [[ "$PROMPT_COMMAND" =~ "_omp_hook" ]]; then
     PROMPT_COMMAND="_omp_hook; $PROMPT_COMMAND"
 fi
-
-function _omp_runonexit() {
-  [[ -f $TIMER_START ]] && rm -f "$TIMER_START"
-}
-
-trap _omp_runonexit EXIT
