de6682d957ce526a68076120a2a8856b82a95454
Riven Skaye <riven@tae.moe>
2022-09-13 08:21:46 +0200

    Add clink stuff for pwetty CMD

---
+++ clink/flexprompt_config.lua
@@ -0,0 +1,24 @@
+flexprompt = flexprompt or {}
+flexprompt.settings = flexprompt.settings or {}
+flexprompt.settings.flow = "fluent"
+flexprompt.settings.symbols = {
+    prompt = {
+        ">",
+        winterminal = "❯"
+    }
+}
+flexprompt.settings.separators = "pointed"
+flexprompt.settings.tails = "flat"
+flexprompt.settings.right_frame = "none"
+flexprompt.settings.left_prompt = "{battery:breakright}{histlabel}{cwd}{git}"
+flexprompt.settings.powerline_font = true
+flexprompt.settings.spacing = "sparse"
+flexprompt.settings.left_frame = "none"
+flexprompt.settings.lines = "two"
+flexprompt.settings.use_8bit_color = true
+flexprompt.settings.connection = "disconnected"
+flexprompt.settings.style = "rainbow"
+flexprompt.settings.use_icons = true
+flexprompt.settings.charset = "unicode"
+flexprompt.settings.right_prompt = "{exit}{duration}{time:format=%a %I:%M %p}"
+flexprompt.settings.heads = "pointed"
