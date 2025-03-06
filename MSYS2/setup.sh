f10e72b437359e9b9472eb69b72ec8bab5470bee
Riven Skaye <riven@tae.moe>
2025-03-06 11:38:26 +0100

    Actually it might be better to just ignore dayjob configs instead

--- MSYS2/setup.sh
+++ MSYS2/setup.sh
@@ -3,29 +3,29 @@
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/bash" &> /dev/null
 
 rm -f $HOME/.{bash_aliases,bash_profile,bashrc,envvars,stars.sh}
-ln -s ./.bash_aliases $HOME/.bash_aliases
-ln -s ./.bash_profile $HOME/.bash_profile
-ln -s ./.bashrc $HOME/.bashrc
-ln -s ./.envvars $HOME/.envvars
-ln -s ./.stars.sh $HOME/.stars.sh
+ln -s $PWD/.bash_aliases $HOME/.bash_aliases
+ln -s $PWD/.bash_profile $HOME/.bash_profile
+ln -s $PWD/.bashrc $HOME/.bashrc
+ln -s $PWD/.envvars $HOME/.envvars
+ln -s $PWD/.stars.sh $HOME/.stars.sh
 
 cd ..
-ln -s .vimrc $HOME/.vimrc
+ln -s $PWD/.vimrc $HOME/.vimrc
 
 clink_dir=$(cygpath -ua $APPDATA/../Local/clink)
 rm -f $clink_dir/{flexprompt_config.lua,clink_settings}
-ln -s ./clink/clink_settings $clink_dir/clink_settings
-ln -s ./clink/flexprompt_config.lua $clink_dir/flexprompt_config.lua
+ln -s $PWD/clink/clink_settings $clink_dir/clink_settings
+ln -s $PWD/clink/flexprompt_config.lua $clink_dir/flexprompt_config.lua
 
-cd ../git
-ln -s ./gitconfig-windows $HOME/.gitconfig
-ln -s ./defaultgitcfg $HOME/.defaultgitcfg
+cd ./git
+ln -s $PWD/.gitconfig-windows $HOME/.gitconfig
+ln -s $PWD/.defaultgitcfg $HOME/.defaultgitcfg
 
 cd ..
 if [[ -d "$HOME/.config" ]]; then
     mv $HOME/.config $HOME/.config_backup
 fi
-ln -s ./.config $HOME/.config
+ln -s $PWD/.config $HOME/.config
 if [[ -d "$HOME/.config_backup" ]]; then
     mv $HOME/.config_backup/* $HOME/.config
 fi
